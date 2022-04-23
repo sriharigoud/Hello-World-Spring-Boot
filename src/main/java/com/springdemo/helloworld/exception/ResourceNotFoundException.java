1e9f56aba8ed7e819488f2ed0cd381ab4e38e67f
srihari <srihari.goud2010@gmail.com>
2022-04-23 20:54:39 +0530

    first commit

---
+++ src/main/java/com/springdemo/helloworld/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.springdemo.helloworld.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String messsage){
+        super(messsage);
+    }
+}
